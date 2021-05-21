e6fce5e2a6963eb9695c77d7948db9d3dce95344
Rosy Patel <rosypatel109@hotmail.com>
2021-05-21 12:37:22 +0100

    Initial Commit

---
+++ CryptoCoin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CryptoCoin
+//
+//  Created by Rosy Patel on 21/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
